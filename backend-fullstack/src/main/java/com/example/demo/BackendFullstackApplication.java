4da2c65ae559a23a72373bef8e4b1c880bac355e
TEJAS7107 <tejaskadam7107@gmail.com>
2023-08-05 17:02:34 +0530

    delete functionality added

--- backend-fullstack/src/main/java/com/example/demo/BackendFullstackApplication.java
+++ backend-fullstack/src/main/java/com/example/demo/BackendFullstackApplication.java
@@ -9,9 +9,10 @@ public class BackendFullstackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendFullstackApplication.class, args);
-		
-		//UsernamePasswordAuthenticationToken data = new UsernamePasswordAuthenticationToken("Tejas710", "Tejas@2");
-		//System.out.println(data);
+
+		// UsernamePasswordAuthenticationToken data = new
+		// UsernamePasswordAuthenticationToken("Tejas710", "Tejas@2");
+		// System.out.println(data);
 	}
 
 }
--- backend-fullstack/src/main/java/com/example/demo/controller/Event_controller.java
+++ backend-fullstack/src/main/java/com/example/demo/controller/Event_controller.java
@@ -9,6 +9,7 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,38 +24,45 @@ import com.example.demo.model.Eventdetail;
 @RestController
 @RequestMapping("api/event")
 public class Event_controller {
-	
+
 	@Autowired
 	private EventData info;
-	
+
 	@PostMapping("/add_event")
 	public String AddEvent(@RequestBody Eventdetail details) {
-		
+
 		details.setCreationDate(new Date());
 		details.setEventId(UUID.randomUUID().toString().split("-")[0]);
 		info.save(details);
 		return details.getEventId();
 	}
-	
+
 	@GetMapping("/get_event_data/{id}")
 	public List<Eventdetail> getAlldataForUser(@PathVariable String id) {
-//		Optional<Eventdetail> data = info.findAllByUserId(id);
+		// Optional<Eventdetail> data = info.findAllByUserId(id);
 		List<Eventdetail> al = new ArrayList<>();
 		List<Eventdetail> getdata = info.findAll();
-		for(int i=0;i<getdata.size();i++) {
-			if (getdata.get(i).getUserId()!=null) {
+		for (int i = 0; i < getdata.size(); i++) {
+			if (getdata.get(i).getUserId() != null) {
 				if (getdata.get(i).getUserId().equalsIgnoreCase(id)) {
-					
+
 					al.add(getdata.get(i));
-					
+
 				}
 			}
-			
-			
+
 		}
-		
-		
+
 		return al;
+	}
+
+	@DeleteMapping("/delete_event/{id}")
+	public String DeleteEvent(@PathVariable String id) {
+
+		info.deleteById(id);
+
+		return "data Deleted successfully";
+
 	}
 
 }
